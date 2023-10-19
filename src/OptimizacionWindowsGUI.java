f69b4e1c0c1f6ba2c0f8654f04f55828aa62e8ae
OMGSAMUELRBR <samueldynamo11@gmail.com>
2023-10-19 15:21:20 +0200

    Falta barra de progreso en desfragmentacion de unidades

--- src/OptimizacionWindowsGUI.java
+++ src/OptimizacionWindowsGUI.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.plaf.ProgressBarUI;
 import java.awt.event.*;
 public class OptimizacionWindowsGUI {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -129,6 +130,29 @@ public class OptimizacionWindowsGUI {
                 } catch (IOException | InterruptedException ioException) {
                     ioException.printStackTrace();
                     JOptionPane.showMessageDialog(null, "Error al desinstalar OneDrive");
+                }
+            }
+        });
+
+        botonDesfragmentarUnidades.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String opcionElegidaDesfragmentacionUnidades = JOptionPane.showInputDialog(null, "Introduzca la unidad a desfragmentar (C, D, E, etc...) en mayusculas", "Desfragmentar unidades", JOptionPane.QUESTION_MESSAGE);
+
+                if (opcionElegidaDesfragmentacionUnidades != null) {
+                    // Creamos el proceso para desfragmentar unidades
+                    ProcessBuilder desfragmentarUnidades = new ProcessBuilder("cmd.exe", "/c", "defrag " + opcionElegidaDesfragmentacionUnidades + " /U /V");
+                    try {
+                        // Ejecutamos el proceso, FALTA: CREAR UNA BARRA DE PROGRESO PROGRESIVA PARA QUE EL USUARIO SEPA QUE ESTA OCURRIENDO
+                        Process desfragmentarUnidadesProcess = desfragmentarUnidades.start();
+                        desfragmentarUnidadesProcess.waitFor();
+                        JOptionPane.showMessageDialog(null, "Unidad " + opcionElegidaDesfragmentacionUnidades + " desfragmentada con exito");
+                    } catch (IOException | InterruptedException ioException) {
+                        ioException.printStackTrace();
+                        JOptionPane.showMessageDialog(null, "Error al desfragmentar la unidad " + opcionElegidaDesfragmentacionUnidades);
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "No se ha introducido ninguna unidad", "Error", JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
