2536693832f16f0d1e233c09744985e53b4e7781
OMGSAMUELRBR <samueldynamo11@gmail.com>
2023-10-24 14:16:00 +0200

    Creado archivo java para ejecutar el Desfragmentador de unidades

--- src/OptimizacionWindowsGUI.java
+++ src/OptimizacionWindowsGUI.java
@@ -31,9 +31,8 @@ public class OptimizacionWindowsGUI {
         botonDesinstalarOneDrive.DesactivarOneDrive(frame);
 
         // Boton para desfragmentar unidades
-        JButton botonDesfragmentarUnidades = new JButton("Desfragmentar unidades");
-        botonDesfragmentarUnidades.setBounds(10, 200, 178, 30);
-        frame.add(botonDesfragmentarUnidades);
+        OpcionesOptimizacion.General.DesfragmentarUnidades botonDesfragmentarUnidades = new OpcionesOptimizacion.General.DesfragmentarUnidades();
+        botonDesfragmentarUnidades.DesfragmentarUnidades(frame);
 
         // Desactivar Bluetooth --
         OpcionesOptimizacion.Servicios.DesactivarBluetooth botonDesactivarBluetooth = new OpcionesOptimizacion.Servicios.DesactivarBluetooth();
@@ -77,29 +76,6 @@ public class OptimizacionWindowsGUI {
                 } catch (IOException | InterruptedException ioException) {
                     ioException.printStackTrace();
                     JOptionPane.showMessageDialog(null, "Error al eliminar la cache");
-                }
-            }
-        });
-
-        botonDesfragmentarUnidades.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String opcionElegidaDesfragmentacionUnidades = JOptionPane.showInputDialog(null, "Introduzca la unidad a desfragmentar (C, D, E, etc...) en mayusculas", "Desfragmentar unidades", JOptionPane.QUESTION_MESSAGE);
-
-                if (opcionElegidaDesfragmentacionUnidades != null) {
-                    // Creamos el proceso para desfragmentar unidades
-                    ProcessBuilder desfragmentarUnidades = new ProcessBuilder("cmd.exe", "/c", "defrag " + opcionElegidaDesfragmentacionUnidades + " /U /V");
-                    try {
-                        // Ejecutamos el proceso, FALTA: CREAR UNA BARRA DE PROGRESO PROGRESIVA PARA QUE EL USUARIO SEPA QUE ESTA OCURRIENDO
-                        Process desfragmentarUnidadesProcess = desfragmentarUnidades.start();
-                        desfragmentarUnidadesProcess.waitFor();
-                        JOptionPane.showMessageDialog(null, "Unidad " + opcionElegidaDesfragmentacionUnidades + " desfragmentada con exito");
-                    } catch (IOException | InterruptedException ioException) {
-                        ioException.printStackTrace();
-                        JOptionPane.showMessageDialog(null, "Error al desfragmentar la unidad " + opcionElegidaDesfragmentacionUnidades);
-                    }
-                } else {
-                    JOptionPane.showMessageDialog(null, "No se ha introducido ninguna unidad", "Error", JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
