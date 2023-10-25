02141f0b7d29a22f1324c84acbbd92e9c8ede3d5
Darkenight07 <samueldynamo11@gmail.com>
2023-10-25 23:43:55 +0200

    Funcionando, falta hacer una comprobacion para ver si esta en ejecuccion

--- src/OpcionesOptimizacion/Servicios/DesactivarWindowsUpdate.java
+++ src/OpcionesOptimizacion/Servicios/DesactivarWindowsUpdate.java
@@ -1,7 +1,11 @@
 package OpcionesOptimizacion.Servicios;
+import java.io.BufferedReader;
 import java.io.IOException;
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class DesactivarWindowsUpdate {
 	
 	public static void DesactivarWindowsUpdate(JFrame frame) {
@@ -13,12 +17,38 @@ public class DesactivarWindowsUpdate {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// Falta
-				
-			}
+				// Creamos el proceso para detener Windows Update;
+
+				String[] comandos = {
+						"cmd.exe",
+						"/C",
+						"net stop wuauserv && sc config wuauserv start=disabled"
+				};
+
+				ProcessBuilder desactivarWindowsUpdate = new ProcessBuilder(comandos);
+
+				try {
+					Process desactivarWindowsUpdateProcess = desactivarWindowsUpdate.start();
+					desactivarWindowsUpdate.redirectErrorStream(true);
+
+					int exitCode = desactivarWindowsUpdateProcess.waitFor();
+					if (exitCode == 0) {
+						JOptionPane.showMessageDialog(null, "Windows Update desactivado correctamente", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+
+					} else {
+						System.out.println(exitCode);
+						JOptionPane.showMessageDialog(null, "Error al desactivar Windows Update", "Error", JOptionPane.ERROR_MESSAGE);
+					}
+				} catch (IOException ex) {
+					JOptionPane.showMessageDialog(null, "Error", "Error", JOptionPane.ERROR_MESSAGE);
+					throw new RuntimeException(ex);
+				} catch (InterruptedException ex) {
+					JOptionPane.showMessageDialog(null, "Error", "Error", JOptionPane.ERROR_MESSAGE);
+
+					throw new RuntimeException(ex);
+                }
+            }
         	
         });
-        
 	}
-
 }
