01cc82897c7ae0b93b4f98e9f733e336ade364a4
Darkenight07 <samueldynamo11@gmail.com>
2023-10-26 12:05:15 +0200

    Funcionando, verificador de estado funcionando

--- src/OpcionesOptimizacion/Servicios/DesactivarWindowsSearch.java
+++ src/OpcionesOptimizacion/Servicios/DesactivarWindowsSearch.java
@@ -1,5 +1,5 @@
 package OpcionesOptimizacion.Servicios;
-import java.io.IOException;
+import OpcionesOptimizacion.VerificarEstadoServicio;
 import javax.swing.*;
 import java.awt.event.*;
 public class DesactivarWindowsSearch {
@@ -13,35 +13,73 @@ public class DesactivarWindowsSearch {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// Falta saber si esta desactivado o activado
 
-				String[] comandos = {
-						"cmd.exe",
-						"/C",
-						"sc stop wsearch && sc config wsearch start=disabled" // auto para activar
-				};
-				try {
-					ProcessBuilder desactivarWindowsSearchPaso1 = new ProcessBuilder(comandos);
-					Process desactivarWindowsSearchPaso1Process = desactivarWindowsSearchPaso1.start();
-					desactivarWindowsSearchPaso1.redirectErrorStream(true);
-					
-					int exitCode = desactivarWindowsSearchPaso1Process.waitFor();
-					
-					if (exitCode == 0) {
-						
-						JOptionPane.showMessageDialog(null, "Se ha desactivado Windows Search correctamente", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+				String estado = VerificarEstadoServicio.VerificarEstadoServicio("wsearch");
 
-					} else {
-						JOptionPane.showMessageDialog(null, "Ha habido un problema al parar el servicio Windows Search", "Error", JOptionPane.ERROR_MESSAGE);
+				if (estado != null) {
+					System.out.println("Estado del servicio Windows Search: " + estado);
+					if (estado.equals("4")) {
+						String[] comandos = {
+								"cmd.exe",
+								"/C",
+								"sc stop wsearch && sc config wsearch start=disabled" // auto para activar
+						};
+
+						try {
+							ProcessBuilder desactivarWindowsSearchPaso1 = new ProcessBuilder(comandos);
+							Process desactivarWindowsSearchPaso1Process = desactivarWindowsSearchPaso1.start();
+							desactivarWindowsSearchPaso1.redirectErrorStream(true);
+
+							int exitCode = desactivarWindowsSearchPaso1Process.waitFor();
+
+							if (exitCode == 0) {
+
+								JOptionPane.showMessageDialog(null, "Se ha desactivado Windows Search correctamente", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+
+							} else {
+								JOptionPane.showMessageDialog(null, "Ha habido un problema al parar el servicio Windows Search", "Error", JOptionPane.ERROR_MESSAGE);
+							}
+						} catch (Exception ex) {
+							JOptionPane.showMessageDialog(null, "Error", "Error", JOptionPane.ERROR_MESSAGE);
+
+						}
+
+					} else if (estado.equals("1")) {
+						JOptionPane.showMessageDialog(null, "El servicio Windows Search ya esta desactivado", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+						int opcion = JOptionPane.showOptionDialog(null, "¿Desea activar el servicio Windows Search?", "Informacion", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[] { "Aceptar", "Cancelar" }, "Aceptar");
+
+						if (opcion == JOptionPane.YES_OPTION) {
+							String[] comandos = {
+									"cmd.exe",
+									"/C",
+									"sc config wsearch start=auto && sc start wsearch"
+							};
+
+							try {
+								ProcessBuilder desactivarWindowsSearchPaso1 = new ProcessBuilder(comandos);
+								Process desactivarWindowsSearchPaso1Process = desactivarWindowsSearchPaso1.start();
+								desactivarWindowsSearchPaso1.redirectErrorStream(true);
+
+								int exitCode = desactivarWindowsSearchPaso1Process.waitFor();
+
+								if (exitCode == 0) {
+									JOptionPane.showMessageDialog(null, "Se ha activado Windows Search correctamente", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+								} else {
+									JOptionPane.showMessageDialog(null, "Ha habido un problema al activar el servicio Windows Search", "Error", JOptionPane.ERROR_MESSAGE);
+								}
+							} catch (Exception ex) {
+								JOptionPane.showMessageDialog(null, "Error", "Error", JOptionPane.ERROR_MESSAGE);
+							}
+						} else if (opcion == JOptionPane.NO_OPTION) {
+
+						}
 					}
-				} catch (Exception ex) {
-					JOptionPane.showMessageDialog(null, "Error", "Error", JOptionPane.ERROR_MESSAGE);
+				} else {
+					JOptionPane.showMessageDialog(null, "Ha habido un problema al verificar el estado del servicio Windows Search", "Error", JOptionPane.ERROR_MESSAGE);
+					System.out.println("No se pudo obtener información del servicio SysMain.");
 
 				}
-				
 			}
-        	
         });
 	}
-
 }
