4e1b07b70e6811202269ec0f76f8545be217e651
Darkenight07 <samueldynamo11@gmail.com>
2023-10-26 11:48:03 +0200

    Funciona, verificador de estado de servicios

---
+++ src/OpcionesOptimizacion/VerificarEstadoServicio.java
@@ -0,0 +1,28 @@
+package OpcionesOptimizacion;
+import OpcionesOptimizacion.General.*;
+import OpcionesOptimizacion.Servicios.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class VerificarEstadoServicio {
+
+    public static String VerificarEstadoServicio(String Proceso) {
+        try {
+
+            ProcessBuilder processBuilder = new ProcessBuilder("sc", "query", Proceso);
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("ESTADO")) {
+                    return line.split("\\s+")[3]; // Obtenemos el estado, 1 y no se esta ejecutando y 4 se esta ejecutando
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
