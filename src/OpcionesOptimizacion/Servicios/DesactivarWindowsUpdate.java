af8e17123aad3fce57151e632f68407c8694b70a
Darkenight07 <66468335+Darkenight07@users.noreply.github.com>
2023-10-27 16:56:50 +0200

    Update DesactivarWindowsUpdate.java

--- src/OpcionesOptimizacion/Servicios/DesactivarWindowsUpdate.java
+++ src/OpcionesOptimizacion/Servicios/DesactivarWindowsUpdate.java
@@ -1,5 +1,5 @@
 package OpcionesOptimizacion.Servicios; 
-import OpcionesOptimizacion.*;
+import OpcionesOptimizacion.*; 
 import java.io.IOException;
 import javax.swing.*;
 import java.awt.event.*;
