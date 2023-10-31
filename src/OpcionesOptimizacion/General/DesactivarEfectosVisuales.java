524d489ddb0bdfaa625d628713ee026c1cc63f44
Darkenight07 <samueldynamo11@gmail.com>
2023-10-31 15:40:29 +0100

    Falta codigo, no terminado

--- src/OpcionesOptimizacion/General/DesactivarEfectosVisuales.java
+++ src/OpcionesOptimizacion/General/DesactivarEfectosVisuales.java
@@ -1,5 +1,11 @@
 package OpcionesOptimizacion.General;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class DesactivarEfectosVisuales {
 
@@ -7,6 +13,25 @@ public class DesactivarEfectosVisuales {
         JButton botonDesactivarEfectosVisuales = new JButton("Desactivar Efectos Visuales");
         botonDesactivarEfectosVisuales.setBounds(250, 160, 300, 30);
         frame.add(botonDesactivarEfectosVisuales);
+
+        botonDesactivarEfectosVisuales.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                int valorActivarRegistro = 3;
+                int valorDesactivarRegistro = 2;
+
+                // src/OpcionesOptimizacion/General/bat/DesactivarEfectosVisuales/prueba.bat
+
+                try {
+                    String[] command = { "cmd", "/k","start", "start",".\\src\\OpcionesOptimizacion\\General\\bat\\DesactivarEfectosVisuales\\prueba.bat" };
+                    Process process = Runtime.getRuntime().exec(command);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+        });
 
     }
 }
