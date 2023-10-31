67ade01534bb922627debafbd48f0c76b797be30
Darkenight07 <samueldynamo11@gmail.com>
2023-10-31 13:45:51 +0100

    Cambios

--- src/OpcionesOptimizacion/General/DesactivarEfectosVisuales.java
+++ src/OpcionesOptimizacion/General/DesactivarEfectosVisuales.java
@@ -4,7 +4,9 @@ import javax.swing.*;
 public class DesactivarEfectosVisuales {
 
     public static void DesactivarEfectosVisuales(JFrame frame) {
-
+        JButton botonDesactivarEfectosVisuales = new JButton("Desactivar Efectos Visuales");
+        botonDesactivarEfectosVisuales.setBounds(250, 160, 300, 30);
+        frame.add(botonDesactivarEfectosVisuales);
 
     }
 }
