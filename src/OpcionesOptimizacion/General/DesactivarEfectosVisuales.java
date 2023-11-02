b901665486521b65060cc1e1c20d916ac7d29b04
Darkenight07 <samueldynamo11@gmail.com>
2023-11-02 14:08:54 +0100

    Funcionando, desactivador de efectos visuales terminado

--- src/OpcionesOptimizacion/General/DesactivarEfectosVisuales.java
+++ src/OpcionesOptimizacion/General/DesactivarEfectosVisuales.java
@@ -2,7 +2,9 @@ package OpcionesOptimizacion.General;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
+import com.sun.jna.platform.win32.Advapi32Util;
+import com.sun.jna.platform.win32.WinReg;
+
 
 public class DesactivarEfectosVisuales {
 
@@ -15,11 +17,38 @@ public class DesactivarEfectosVisuales {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                int valorActivarRegistro = 3;
-                int valorDesactivarRegistro = 2;
+
+                // HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\VisualEffects
+                // VisualFXSetting
+
+                final int VALOR_ACTIVADO = 3;
+                final int VALOR_DESACTIVADO = 2;
+
+                int valorVisualFXSetting = Advapi32Util.registryGetIntValue(WinReg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\VisualEffects", "VisualFXSetting");
+
+                if (valorVisualFXSetting == 3 ) {
+                    int confirm = JOptionPane.showConfirmDialog(null, "Los efectos visuales ya estan activados, ¿Desea desactivarlos", "Confirmar", JOptionPane.YES_NO_OPTION);
+
+                    if (confirm == JOptionPane.YES_OPTION) {
+                        Advapi32Util.registrySetIntValue(WinReg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\VisualEffects", "VisualFXSetting", VALOR_DESACTIVADO);
+                        JOptionPane.showMessageDialog(null, "Efectos visuales desactivados correctamente", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+                    } else if (confirm == JOptionPane.NO_OPTION) {
+                        JOptionPane.showMessageDialog(null, "No se han desactivado los efectos visuales", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                } else if (valorVisualFXSetting == 2) {
+                    int confirm = JOptionPane.showConfirmDialog(null, "Los efectos visuales ya estan desactivados, ¿Desea activarlos", "Confirmar", JOptionPane.YES_NO_OPTION);
+
+                    if (confirm == JOptionPane.YES_OPTION) {
+                        Advapi32Util.registrySetIntValue(WinReg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\VisualEffects", "VisualFXSetting", VALOR_ACTIVADO);
+                        JOptionPane.showMessageDialog(null, "Efectos visuales activados correctamente", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+                    } else if (confirm == JOptionPane.NO_OPTION) {
+                        JOptionPane.showMessageDialog(null, "No se han activado los efectos visuales", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "No se ha podido comprobar el estado de los efectos visuales", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+                }
 
             }
         });
-
     }
 }
